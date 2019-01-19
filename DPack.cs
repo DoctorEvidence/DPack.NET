d92c3a43ab6e5c4a21fc9e9281e3c2756c8f90a3
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2019-01-19 10:29:27 -0500

    Initial commit.

---
+++ DPack.cs
@@ -0,0 +1,56 @@
+﻿using Jering.Javascript.NodeJS;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DPack.NET
+{
+    public class DPack
+    {
+        private ServiceCollection _servicesCollection;
+        private ServiceProvider _serviceProvider;
+        private string _DPackJs;
+
+        public DPack()
+        {
+            _servicesCollection = new ServiceCollection();
+            _servicesCollection.AddNodeJS();
+            _serviceProvider = _servicesCollection.BuildServiceProvider();
+            _DPackJs = File.ReadAllText(@"..\node_modules\dpack\dist\index.js");
+        }
+
+        public async Task<string> Serialize(string input)
+        {
+            string output;
+            try
+            {
+                output = await StaticNodeJSService.InvokeFromStringAsync<string>(_DPackJs, "serialize", args: new object[] { input });
+            }
+            catch (Exception ex)
+            {
+                output = ex.Message;
+            }
+
+            return output;
+        }
+
+        public async Task<string> Parse(string input)
+        {
+            string output;
+            try
+            {
+                output = await StaticNodeJSService.InvokeFromStringAsync<string>(_DPackJs, "parse", args: new object[] { input });
+            }
+            catch (Exception ex)
+            {
+                output = ex.Message;
+            }
+
+            return output;
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# DPack.NET
+A .NET implementation of [dpack](https://github.com/DoctorEvidence/dpackjs)
+
+## Specification
+The [specification of the dpack format is available here](https://github.com/DoctorEvidence/dpack).
